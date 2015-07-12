1d7342173f9fc9d5f01dcd65538b3a0cb7bac4be
Rohit Datta <rohitdatta11@gmail.com>
2015-07-12 17:06:09 -0500

    Base outline for tabbed view controllers

--- HackTX/TwitterViewController.swift
+++ HackTX/TwitterViewController.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import UIKit
+
+class TwitterViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+}
